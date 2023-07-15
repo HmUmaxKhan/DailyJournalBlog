f7b4357039bdfb928271e066667807d7203f4086
Muhammad Umar Khan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-15 21:42:32 +0500

    Initial Commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const {mongoose} = require("mongoose");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb+srv://admin:408126@cluster0.yhubrpl.mongodb.net/todoDB?retryWrites=true&w=majority", {useNewUrlParser: true},{ useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin:408126@cluster0.yhubrpl.mongodb.net/todoDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -104,11 +105,7 @@ app.get("/posts/:postName", function(req, res){
   console.log("Server started on port 3000");
 });` is starting the server and listening for incoming requests on port 3000. When the server starts
 successfully, it will log the message "Server started on port 3000" to the console. */
-let port = process.env.PORT
 
-if (port === null || port === "") {
-  port = 3000;
-}
-app.listen(port, function() {
+app.listen(3000, function() {
   console.log("Server started on port Heroku");
 });
